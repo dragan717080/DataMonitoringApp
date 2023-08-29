b140287da3dba3c604863a90337ab0ec0b02ab26
= <hardlane2000@gmail.com>
2023-08-29 09:29:18 +0200

    adding media query for the cloud component

--- app/(site)/components/Cloud.tsx
+++ app/(site)/components/Cloud.tsx
@@ -4,10 +4,11 @@ import '../../styles/cloud.css';
 
 const Cloud: FC = () => {
   return (
-    <div className='my-10 w-full absolute left-0 h-[40rem] overflow-hidden'>
-      <div className="col-v absolute h-[80%] w-[60%] row top-[10%] left-[20%] z-10">
+    <div className='my-10 w-full absolute left-0 h-[53rem] 2xl:h-[40rem] overflow-hidden'>
+      <div className="col-v absolute h-[80%] w-[90%] 2xl:w-[60%] row top-[10%] left-[5%] 2xl:left-[20%] z-10">
         <div className='bg-gray-600 h-full w-full rounded-xl'>
-          <div className="grid grid-cols-2 gap-x-10 h-full">
+          {/* for large screens */}
+          <div className="hidden md:grid grid-cols-2 md:gap-x-4 xl:gap-x-7 2xl:gap-x-10 h-full">
             <div className="cloud-card flex flex-col items:center px-12 my-10">
               <div className='cloud-card-title bold'>
                 <i>Serverless actions</i>
@@ -26,23 +27,23 @@ const Cloud: FC = () => {
               </div>
             </div>
             <div className='flex flex-col items:center '>
-              <div className='relative mx-auto h-[12rem] w-[20rem] mt-[3.25rem] mb-[7.5rem]'>
+              <div className='relative mx-auto h-[12rem] w-[20rem] md:w-[18rem] lg:w-[20rem] mt-[3.25rem] mb-[7.375rem]'>
                 <Image
                   height='440'
                   width='320'
                   alt='Logo'
                   src='/assets/images/aws-lambda.png'
                   objectFit='cover'
-                  className='h-full w-full'
+                  className='h-full w-full rounded-md 2xl:rounded-lg'
                 />
-                <div className='absolute h-[10rem] w-[15rem] 2xl:top-[60%] 2xl:left-[-25%]'>
+                <div className='absolute h-[10rem] w-[15rem] md:top-[60%] md:left-[-12%] xl:left-[-20%] 2xl:left-[-25%]'>
                   <Image
                     height='440'
                     width='320'
                     alt='Logo'
                     src='/assets/images/aws-lambda-1.png'
                     objectFit='cover'
-                    className='h-full w-full'
+                    className='h-full w-full rounded-md 2xl:rounded-lg'
                   />
                 </div>
               </div>
@@ -51,33 +52,68 @@ const Cloud: FC = () => {
               </div>
             </div>
           </div>
+          {/* for small screens */}
+          <div className="md:hidden flex flex-col items-center h-full w-full">
+            <div className='cloud-card-title bold mt-6'>
+              <i>Serverless actions</i>
+            </div>
+            <div className='flex flex-col items:center '>
+              <div className='relative mx-auto h-[12rem] w-[18rem] 2xl:w-[20rem] mt-[3.25rem] mb-[3.5rem] 2xl:mb-[7.375rem]'>
+                <Image
+                  height='440'
+                  width='320'
+                  alt='Logo'
+                  src='/assets/images/aws-lambda.png'
+                  objectFit='cover'
+                  className='h-full w-full rounded-md 2xl:rounded-lg'
+                />
+                <div className='hidden absolute h-[10rem] w-[15rem] 2xl:top-[60%] 2xl:left-[-25%]'>
+                  <Image
+                    height='440'
+                    width='320'
+                    alt='Logo'
+                    src='/assets/images/aws-lambda-1.png'
+                    objectFit='cover'
+                    className='h-full w-full rounded-md 2xl:rounded-lg'
+                  />
+                </div>
+              </div>
+              <div className='cloud-text px-4 md:px-0'>
+                Similar to Azure Functions, AWS Lambda, or GitHub Actions, our platform enables you to run code without managing servers, simplifying your workflow and saving valuable time and resources.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div id="clouds" className='h-full'>
-        <div className="cloud x1"></div>
-        <div className="cloud x2"></div>
-        <div className="cloud x3"></div>
-        <div className="cloud x4"></div>
-        <div className="cloud x5"></div>
-        <div className="cloud x6"></div>
-        <div className="cloud x7"></div>
-        <div className="cloud x8"></div>
-        <div className="cloud x9"></div>
-        <div className="cloud x10"></div>
-        <div className="cloud x11"></div>
-        <div className="cloud x12"></div>
-        <div className="cloud x13"></div>
-        <div className="cloud x14"></div>
-        <div className="cloud x15"></div>
-        <div className="cloud x16"></div>
-        <div className="cloud x17"></div>
-        <div className="cloud x18"></div>
-        <div className="cloud x19"></div>
-        <div className="cloud x20"></div>
-        <div className="cloud x21"></div>
-        <div className="cloud x22"></div>
-        <div className="cloud x23"></div>
-        <div className="cloud x24"></div>
+        <div className="cloud x1" />
+        <div className="cloud x2" />
+        <div className="cloud x3" />
+        <div className="cloud x4" />
+        <div className="cloud x5" />
+        <div className="cloud x6" />
+        <div className="cloud x7" />
+        <div className="cloud x8" />
+        <div className="cloud x9" />
+        <div className="cloud x10" />
+        <div className="cloud x11" />
+        <div className="cloud x12" />
+        <div className="cloud x13" />
+        <div className="cloud x14" />
+        <div className="cloud x15" />
+        <div className="cloud x16" />
+        <div className="cloud x17" />
+        <div className="cloud x18" />
+        <div className="cloud x19" />
+        <div className="cloud x20" />
+        <div className="cloud x21" />
+        <div className="cloud x22" />
+        <div className="cloud x23" />
+        <div className="cloud x24" />
+        <div className="cloud x25" />
+        <div className="cloud x26" />
+        <div className="cloud x27" />
+        <div className="cloud x28" />
       </div>
     </div>
   )
